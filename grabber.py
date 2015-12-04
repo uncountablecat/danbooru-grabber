251706b11844c1a1b51cb770142f5d628bedb7e4
Chao <chaoguo@wifi-131-179-40-88.host.ucla.edu>
2015-12-03 16:12:35 -0800

    Users can now decide the number of posts they want to retrieve.

--- grabber.py
+++ grabber.py
@@ -5,9 +5,9 @@ import urllib as urllib
 import requests
 
 # change this to your danbooru folder
-# it might look something like this: '/users/YourUserName/GelbooruPics'
+# it might look something like this: '/users/YourUserName/DanbooruPics'
 # make sure the folder already exists!
-danbooru_folder = '/users/NicoNicoNi/danboorupics/'
+danbooru_folder = '/Users/chaoguo/gelboorupics/'
 
 # generate tag argument to be used in url and folder creation
 def generate_tag_argv(tagList):
@@ -19,8 +19,8 @@ def generate_tag_argv(tagList):
 	return tag_argv
 
 # request json, get urls of pictures and download them
-def grabber(tag_argv):
-	r = requests.get('https://danbooru.donmai.us/posts.json?tags='+tag_argv+'&limit=60')
+def grabber(tag_argv,pic_num):
+	r = requests.get('https://danbooru.donmai.us/posts.json?tags='+tag_argv+'&limit='+str(pic_num))
 	streams = r.json()
 
 	# check if directory already exists
@@ -37,11 +37,19 @@ def grabber(tag_argv):
 	for address in target:
 		urllib.urlretrieve(address,danbooru_folder+tag_argv+'/'+address.split('/')[-1])
 
-# you can use input instead of raw_input if you are using Python 3
+pic_num = input('Enter the number of posts you would like to download:')
+while (pic_num >100):
+	print('The number is not valid!')
+	print('Danbooru only allows 100 pictures at most!')
+	u1 = u'健全な生活を送りましょう！'
+	print u1
+	pic_num = input('Enter the number of posts you would like to download:')
+
 tagList = raw_input('Enter tags,separated by space:') 
 tagList = tagList.split(' ')
 tag_argv = generate_tag_argv(tagList)
-grabber(tag_argv)
+grabber(tag_argv,pic_num)
+
 print('Download successful!')
-u = u'召し上がってください！'
-print u
+u2 = u'どうぞ、召し上がってください！'
+print u2
